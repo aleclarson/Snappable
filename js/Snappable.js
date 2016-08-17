9721fd842f762a16e73837e867533aa479d982df
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-08-17 16:09:49 -0700

    08/17/2016 4:09 PM

--- js/Snappable.js
+++ js/Snappable.js
@@ -1,10 +1,14 @@
-var Event, NativeValue, SpringAnimation, Type, assertType, clampValue, getSign, mergeDefaults, type;
+var Event, NativeValue, ReactiveVar, SpringAnimation, Type, assertType, assertTypes, clampValue, configTypes, getSign, type;
+
+require("isDev");
 
 NativeValue = require("modx/native").NativeValue;
 
 SpringAnimation = require("SpringAnimation");
 
-mergeDefaults = require("mergeDefaults");
+ReactiveVar = require("ReactiveVar");
+
+assertTypes = require("assertTypes");
 
 assertType = require("assertType");
 
@@ -17,13 +21,12 @@ Type = require("Type");
 type = Type("Snappable");
 
 type.defineOptions({
-  offset: NativeValue.isRequired,
   index: Number.withDefault(0),
   maxIndex: Number.isRequired,
-  gapLength: Number.isRequired,
-  distanceThreshold: Number.isRequired,
-  velocityThreshold: Number.isRequired,
+  gapLength: Number,
   maxVelocity: Number.withDefault(2e308),
+  distanceThreshold: Number.withDefault(0),
+  velocityThreshold: Number.withDefault(0),
   tension: Number.withDefault(8),
   friction: Number.withDefault(5)
 });
@@ -37,58 +40,71 @@ type.defineValues(function(options) {
     maxVelocity: options.maxVelocity,
     tension: options.tension,
     friction: options.friction,
-    willSnap: Event(),
-    _index: null,
-    _offset: options.offset
+    willAnimate: Event(),
+    _index: ReactiveVar(options.index),
+    _offset: NativeValue(0)
   };
 });
 
-type.initInstance(function(options) {
-  return this.index = options.index;
-});
-
 type.defineGetters({
-  restOffset: function() {
-    return this._index * this.gapLength;
-  },
   maxOffset: function() {
-    return this.maxIndex * this.gapLength;
+    return this.__getMaxOffset();
+  },
+  restOffset: function() {
+    return this.offsetAtIndex(this._index._value);
   }
 });
 
 type.definePrototype({
   index: {
     get: function() {
-      return this._index;
+      return this._index.get();
     },
-    set: function(newIndex, oldIndex) {
-      if (newIndex === oldIndex) {
-        return;
-      }
-      this._index = newIndex;
-      return this._offset.value = newIndex * this.gapLength;
+    set: function(index) {
+      return this._index.set(index);
     }
   }
 });
 
 type.defineMethods({
-  toNearest: function(distance, velocity) {
-    var nextIndex;
-    nextIndex = this.resolveIndex(distance, velocity);
-    return this.toIndex(nextIndex, velocity);
-  },
-  toIndex: function(index, velocity) {
+  offsetAtIndex: function(index) {
     assertType(index, Number);
-    assertType(velocity, Number);
-    this.willSnap.emit(index, velocity);
-    this._index = index;
+    return this.__getOffset(index);
+  },
+  animate: function(config) {
+    var fromOffset, restOffset, toIndex;
+    if (isDev) {
+      assertTypes(config, configTypes.animate);
+      if (config.toIndex != null) {
+        if (config.fromOffset == null) {
+          throw Error("Must define 'config.fromOffset' (because 'config.toIndex' is defined)!");
+        }
+      } else if (config.distance == null) {
+        throw Error("Must define 'config.toIndex' or 'config.distance'!");
+      }
+    }
+    if (config.distance != null) {
+      toIndex = this.resolveIndex(config.distance, config.velocity);
+      fromOffset = config.distance + this.offsetAtIndex(this._index._value);
+    } else {
+      toIndex = config.toIndex, fromOffset = config.fromOffset;
+    }
+    restOffset = this.offsetAtIndex(toIndex);
+    this.willAnimate.emit({
+      toIndex: toIndex,
+      fromOffset: fromOffset,
+      restOffset: restOffset
+    }, config);
+    this._index.set(toIndex);
     return this._offset.animate({
       type: SpringAnimation,
-      endValue: index * this.gapLength,
-      velocity: velocity,
       tension: this.tension,
       friction: this.friction,
-      clamp: true
+      clamp: true,
+      endValue: restOffset - fromOffset,
+      velocity: config.velocity,
+      onUpdate: config.onUpdate,
+      onEnd: config.onEnd
     });
   },
   resolveIndex: function(distance, velocity) {
@@ -102,13 +118,22 @@ type.defineMethods({
     velocitySign = getSign(velocity);
     if (velocitySign === distanceSign) {
       if (Math.abs(velocity) > this.velocityThreshold) {
-        return this._index + velocitySign;
+        return this._index.get() + velocitySign;
       }
       if (Math.abs(distance) > this.distanceThreshold) {
-        return this._index + distanceSign;
+        return this._index.get() + distanceSign;
       }
     }
-    return this._index;
+    return this._index.get();
+  }
+});
+
+type.defineHooks({
+  __getOffset: function(index) {
+    return index * this.gapLength;
+  },
+  __getMaxOffset: function() {
+    return this.maxIndex * this.gapLength;
   }
 });
 
@@ -124,5 +149,17 @@ getSign = function(value) {
     return -1;
   }
 };
+
+if (isDev) {
+  configTypes = {};
+  configTypes.animate = {
+    velocity: Number,
+    distance: Number.Maybe,
+    toIndex: Number.Maybe,
+    fromOffset: Number.Maybe,
+    onUpdate: Function.Maybe,
+    onEnd: Function.Maybe
+  };
+}
 
 //# sourceMappingURL=map/Snappable.map
--- src/Snappable.coffee
+++ src/Snappable.coffee
@@ -1,8 +1,11 @@
 
+require "isDev"
+
 {NativeValue} = require "modx/native"
 
 SpringAnimation = require "SpringAnimation"
-mergeDefaults = require "mergeDefaults"
+ReactiveVar = require "ReactiveVar"
+assertTypes = require "assertTypes"
 assertType = require "assertType"
 clampValue = require "clampValue"
 Event = require "Event"
@@ -11,13 +14,12 @@ Type = require "Type"
 type = Type "Snappable"
 
 type.defineOptions
-  offset: NativeValue.isRequired
   index: Number.withDefault 0
   maxIndex: Number.isRequired
-  gapLength: Number.isRequired
-  distanceThreshold: Number.isRequired
-  velocityThreshold: Number.isRequired
+  gapLength: Number
   maxVelocity: Number.withDefault Infinity
+  distanceThreshold: Number.withDefault 0
+  velocityThreshold: Number.withDefault 0
   tension: Number.withDefault 8
   friction: Number.withDefault 5
 
@@ -37,57 +39,65 @@ type.defineValues (options) ->
 
   friction: options.friction
 
-  willSnap: Event()
+  willAnimate: Event()
 
-  _index: null
+  _index: ReactiveVar options.index
 
-  _offset: options.offset
-
-type.initInstance (options) ->
-
-  @index = options.index
+  _offset: NativeValue 0
 
 type.defineGetters
 
-  restOffset: -> @_index * @gapLength
+  maxOffset: -> @__getMaxOffset()
 
-  maxOffset: -> @maxIndex * @gapLength
+  restOffset: -> @offsetAtIndex @_index._value
 
 type.definePrototype
 
   index:
-    get: -> @_index
-    set: (newIndex, oldIndex) ->
-      return if newIndex is oldIndex
-      @_index = newIndex
-      @_offset.value = newIndex * @gapLength
+    get: -> @_index.get()
+    set: (index) ->
+      @_index.set index
 
 type.defineMethods
 
-  toNearest: (distance, velocity) ->
-    nextIndex = @resolveIndex distance, velocity
-    return @toIndex nextIndex, velocity
-
-  toIndex: (index, velocity) ->
-
+  offsetAtIndex: (index) ->
     assertType index, Number
-    assertType velocity, Number
-    @willSnap.emit index, velocity
+    return @__getOffset index
 
-    @_index = index
-    return @_offset.animate {
+  animate: (config) ->
+    if isDev
+      assertTypes config, configTypes.animate
+      if config.toIndex?
+        if not config.fromOffset?
+          throw Error "Must define 'config.fromOffset' (because 'config.toIndex' is defined)!"
+      else if not config.distance?
+        throw Error "Must define 'config.toIndex' or 'config.distance'!"
+
+    if config.distance?
+      toIndex = @resolveIndex config.distance, config.velocity
+      fromOffset = config.distance + @offsetAtIndex @_index._value
+    else {toIndex, fromOffset} = config
+
+    restOffset = @offsetAtIndex toIndex
+
+    @willAnimate.emit {toIndex, fromOffset, restOffset}, config
+    @_index.set toIndex
+    @_offset.animate {
       type: SpringAnimation
-      endValue: index * @gapLength
-      velocity
       @tension
       @friction
       clamp: yes
+      endValue: restOffset - fromOffset
+      velocity: config.velocity
+      onUpdate: config.onUpdate
+      onEnd: config.onEnd
     }
 
   resolveIndex: (distance, velocity) ->
     index = @_resolveIndex distance, velocity
     return clampValue index, 0, @maxIndex
 
+  # TODO: Support loose snapping.
   _resolveIndex: (distance, velocity) ->
 
     distanceSign = getSign distance
@@ -96,15 +106,37 @@ type.defineMethods
     if velocitySign is distanceSign
 
       if Math.abs(velocity) > @velocityThreshold
-        return @_index + velocitySign
+        return @_index.get() + velocitySign
 
       if Math.abs(distance) > @distanceThreshold
-        return @_index + distanceSign
+        return @_index.get() + distanceSign
 
-    return @_index
+    return @_index.get()
+
+type.defineHooks
+
+  __getOffset: (index) ->
+    index * @gapLength
+
+  __getMaxOffset: ->
+    @maxIndex * @gapLength
 
 module.exports = type.build()
+
+#
+# Helpers
+#
 
 getSign = (value) ->
   return 0 if value is 0
   if value > 0 then 1 else -1
+
+if isDev
+  configTypes = {}
+  configTypes.animate =
+    velocity: Number
+    distance: Number.Maybe
+    toIndex: Number.Maybe
+    fromOffset: Number.Maybe
+    onUpdate: Function.Maybe
+    onEnd: Function.Maybe
